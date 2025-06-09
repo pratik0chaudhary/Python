44642f9dfa6cebd3ca7ad8aee12df70d883458d3
pratik0chaudhary <pratiktharu912@gmail.com>
2025-06-09 14:53:01 +0545

    substract number

---
+++ sub_pratik.py
@@ -0,0 +1,11 @@
+# Subtracting two numbers
+
+# Input from user
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+# Subtract
+result = a - b
+
+# Output the result
+print("The result of subtraction is:", result)

512629b864fa75adc7d47cd65309a4933f092e70
pratik0chaudhary <pratiktharu912@gmail.com>
2025-06-12 09:41:01 +0545

    Update sub_pratik.py

--- sub_pratik.py
+++ sub_pratik.py
@@ -5,7 +5,7 @@ a = float(input("Enter the first number: "))
 b = float(input("Enter the second number: "))
 
 # Subtract
-result = (a - b)*3
+result = (a - b)*7
 
 # Output the result
 print("The result of subtraction is:", result)
